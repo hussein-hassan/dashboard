596f52f18b9c793006ca28d0ee03555e7c3984dd
hussein.hassan@vodafone.com <hussein.hassan@vodafone.com>
2020-02-04 00:07:41 +0200

    fix login backdrop

--- src/app/core/components/modal/modal.component.ts
+++ src/app/core/components/modal/modal.component.ts
@@ -1,15 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    let el = document.querySelector('div.modal-backdrop');
+    el.parentNode.removeChild(el);
   }
 
 }
